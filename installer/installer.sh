f37e4226284944258666f6bed54367edd24c50dd
Patrick Büch <git@paco.pl31.de>
2018-07-18 00:43:03 +0200

    Fix installer

--- installer/installer.sh
+++ installer/installer.sh
@@ -27,13 +27,13 @@ echo >> ~/.profile
 echo "clear; fbterm -r 3 -s 54 -- python3 ~/raspi-netmon-v2/netmon/netmon.py -s 20x13" >> ~/.profile
 
 echo "---> Enable tmpfs for tcpdump"
-sudo cp ~/raspi-netmon/installer/var-run-tcpdump_eth0.mount /etc/systemd/system
+sudo cp ~/raspi-netmon-v2/installer/var-run-tcpdump_eth0.mount /etc/systemd/system
 sudo systemctl enable var-run-tcpdump_eth0.mount
 sudo systemctl start var-run-tcpdump_eth0.mount
 echo "---> Create symbolic links for tcpdump"
 sudo sh -c 'for i in `seq 0 3`; do ln -s /var/run/tcpdump_eth0/tcpdump_eth0_$i /var/www/html/tcpdump_eth0_$i.pcap; done' || true
 echo "---> Enable tcpdump service"
-sudo cp ~/raspi-netmon/installer/tcpdump_eth0.service /etc/systemd/system
+sudo cp ~/raspi-netmon-v2/installer/tcpdump_eth0.service /etc/systemd/system
 sudo systemctl enable tcpdump_eth0.service
 
 echo
