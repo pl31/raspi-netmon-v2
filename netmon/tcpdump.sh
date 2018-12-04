5d952c19056a3121fff1d16cdebf329cd741fb6a
Patrick Büch <git@paco.pl31.de>
2018-12-04 19:28:33 +0100

    Fix escaping

--- netmon/tcpdump.sh
+++ netmon/tcpdump.sh
@@ -5,4 +5,4 @@ if=$1
 filter="not ether host $(cat /sys/class/net/$if/address)"
 echo "Filter: \"$filter\""
 
-/usr/sbin/tcpdump -n -U -s 0 -i $if -W 4 -C 16M -w "/var/run/tcpdump/tcpdump_$if_" "$filter"
+/usr/sbin/tcpdump -n -U -s 0 -i $if -W 4 -C 16M -w '/var/run/tcpdump/tcpdump_'$if'_' "$filter"
