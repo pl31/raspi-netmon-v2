d259a8e7858c0aca6ecf9837e3c2dfb901cbb76c
Patrick Büch <git@paco.pl31.de>
2018-12-04 18:37:46 +0100

    Reduce pcap size

--- netmon/tcpdump.sh
+++ netmon/tcpdump.sh
@@ -5,4 +5,4 @@ if=$1
 filter="not ether host $(cat /sys/class/net/$1/address)"
 echo "Filter: \"$filter\""
 
-/usr/sbin/tcpdump -n -U -s 0 -i $if -W 4 -C 32M -w /var/run/tcpdump/tcpdump_$if_ "$filter"
+/usr/sbin/tcpdump -n -U -s 0 -i $if -W 4 -C 16M -w /var/run/tcpdump/tcpdump_$if_ "$filter"
